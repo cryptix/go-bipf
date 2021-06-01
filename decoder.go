0258d036db69c4fc54fe1bfac8355855749e1fc8
Henry <cryptix@riseup.net>
2021-06-01 19:18:33 +0200

    document the decoder functions

--- decoder.go
+++ decoder.go
@@ -5,35 +5,54 @@ import (
 	"io"
 )
 
+// Decoder holds the internal state of the reader portion of the bipf implementation
 type Decoder struct {
 	input io.ReadSeeker
 }
 
 var errTODO = fmt.Errorf("bipf: todo - not implemented")
 
+// NewDecoder initializes the decoder
 func NewDecoder(rd io.ReadSeeker) *Decoder {
 	return &Decoder{input: rd}
 }
 
-func (d *Decoder) Bool() (bool, error) {
-	return false, errTODO
-}
-func (d *Decoder) CopyString() (string, error) {
-	return "false", errTODO
-}
-func (d *Decoder) Double() (float64, error) {
-	return -1, errTODO
-}
-func (d *Decoder) Int32() (int32, error) {
-	return -1, errTODO
-}
+// Next advances to the next value
 func (d *Decoder) Next() error {
 	return errTODO
 }
 
+// Skip discards the current value
+func (d *Decoder) Skip() error {
+	return errTODO
+}
+
+// SeekToLabel seeks through the stream until it finds a value with that chain of object key names to it
 func (d *Decoder) SeekToLabel(p string) error {
 	return errTODO
 }
+
+// Type returns the type of the current value
 func (d *Decoder) Type() (Type, error) {
 	return 0, errTODO
+}
+
+// Bool returns the value if the current type is a bool
+func (d *Decoder) Bool() (bool, error) {
+	return false, errTODO
+}
+
+// CopyString returns a copy of value if the current type is a string
+func (d *Decoder) CopyString() (string, error) {
+	return "false", errTODO
+}
+
+// Double returns the floating-point value if the current type is a double
+func (d *Decoder) Double() (float64, error) {
+	return -1, errTODO
+}
+
+// Int32 returns a the 32bit integer value if the current type is a integer
+func (d *Decoder) Int32() (int32, error) {
+	return -1, errTODO
 }
